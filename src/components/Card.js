ce891567cffe7cead567d1adcf683db6be7b605c
paguirre82 <pablod@teambaw.com.au>
2022-08-16 15:51:06 +1000

    Creating card component that will contain styles that will decrease code duplication

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import './Card.css'
+
+function Card(props) {
+    const classes = 'card '  + props.className;
+    return <div className={classes}>{props.children}</div> // children = built into react and is what's between open and close tags
+}
+
+export default Card;
