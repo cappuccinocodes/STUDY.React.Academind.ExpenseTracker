123c885316bdbec520dcb9e24d617dbbfb1bdece
paguirre82 <pablod@teambaw.com.au>
2022-08-16 17:22:06 +1000

    form created, still not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -29,6 +30,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css'
 
-function Card(props) {
+const Card = (props) => {
     const classes = 'card '  + props.className;
     return <div className={classes}>{props.children}</div> // children = built into react and is what's between open and close tags
 }
